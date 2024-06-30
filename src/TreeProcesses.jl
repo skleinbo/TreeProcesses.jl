573cfd940db62e0112171f184374516e45219b9a
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2024-06-30 12:15:14 +0200

    Introduce structs to hold node values.
    nichemodel now also returns number of nodes after pruning.
    
    renamed weighted_coalescent to preferential_coalencent

--- src/TreeProcesses.jl
+++ src/TreeProcesses.jl
@@ -1,6 +1,6 @@
 module TreeProcesses
 
-export  ACD!, moran, moran2, birthdeath, weighted_coalescent, coalescent,
+export  ACD!, moran, moran2, birthdeath, preferential_coalescent, coalescent,
         maximally_balanced, maximally_unbalanced, nichemodel, to_mean_dataframe, yule
 
 import AbstractTrees
@@ -9,11 +9,16 @@ import Base: empty!
 using BinaryTrees
 using DataFrames
 using DataStructures: PriorityQueue, dequeue!, enqueue!
+using StaticArrays: MVector, @MVector
 using Statistics: mean
 using StatsBase: sample
 using WeightedSampling: WeightedSampler, adjust_weight!, sample as ws_sample, weight
 
 include("utilities.jl")
+
+mutable struct DefaultNodeValue{T}
+    observables::T
+end
 
 "Distance between a node `i` and its ancestor `j` on a tree."
 function dist(i, j)
@@ -84,17 +89,16 @@ function ACD(p)
     C = 0
     D = 0
     if !isnothing(p.left)
-        A += p.left.val[1]
-        C += p.left.val[2]
-        D += p.left.val[3]
+        A += p.left.val.observables[1]
+        C += p.left.val.observables[2]
+        D += p.left.val.observables[3]
     end
     if !isnothing(p.right)
-        A += p.right.val[1]
-        C += p.right.val[2]
-        D += p.right.val[3]
+        A += p.right.val.observables[1]
+        C += p.right.val.observables[2]
+        D += p.right.val.observables[3]
     end
-    # D = sum(x->x.val[2], children(p)) + A
-    A, C+A, D+A-1
+    return A, C+A, D+A-1
 end
 
 function ACD!(P::BinaryTree, slots=1:3)
@@ -102,13 +106,13 @@ function ACD!(P::BinaryTree, slots=1:3)
     C = Int[]
     D = Int[]
     function agg(P)
-        P.val[slots] .= ACD(P)
-        push!(A, P.val[1])
-        push!(C, P.val[2])
-        push!(D, P.val[3])
+        P.val.observables[slots] .= ACD(P)  
+        push!(A, P.val.observables[1])
+        push!(C, P.val.observables[2])
+        push!(D, P.val.observables[3])
     end
     function init_leaf!(P)
-        P.val[slots] .= (1, 1, 0)
+        P.val.observables[slots] .= (1, 1, 0)
         push!(A, 1)
         push!(C, 1)
         push!(D, 0)
@@ -126,7 +130,7 @@ Simulate the coalescent process for n genes.
 
 Return a directed graph with edges pointing towards the root.
 """
-function coalescent(n; default_value=()->[0,0])
+function coalescent(n; default_value=()->DefaultNodeValue([0,0,0]))
     P = [BinaryTree(default_value()) for _ in 1:n] # start with n vertices; store A,C
     while n > 1
         i, j = sample(1:n, 2, replace=false, ordered=true) #sample two distinct vertices to coalesce
@@ -178,7 +182,7 @@ end
   At each time step a bifurcation event happens at any of the
   current tips with equal probability.
 """
-function yule(n; default_value=()->[0,0])
+function yule(n; default_value=()->DefaultNodeValue([0,0,0]))
     P = [BinaryTree(default_value())]
     k = 1
     while length(P) - k + 1 < n
@@ -191,7 +195,7 @@ function yule(n; default_value=()->[0,0])
 
         k += 1
     end
-    P[1]
+    return P[1]
 end
 
 """
@@ -208,7 +212,7 @@ end
 
   Return an ancestral tree, or a vector of trees if the process hasn't coalesced.
 """
-function birthdeath(n, T, d, b=1.0; N=0, default_value=()->[0,0])
+function birthdeath(n, T, d, b=1.0; N=0, default_value=()->DefaultNodeValue([0,0,0]))
     P = [BinaryTree(default_value()) for _ in 1:n]
     t = 1
     N > 0 && sizehint!(P, N)
@@ -264,7 +268,7 @@ moran(n, T; kwargs...) = birthdeath(n, T, 1.0; kwargs...)
 """
   Return a fully imbalanced binary tree of given height.
 """
-function maximally_unbalanced(height; default_value=()->[0,0,0])
+function maximally_unbalanced(height; default_value=()->DefaultNodeValue([0,0,0]))
     P = BinaryTree(default_value())
     h = 0
     attach_to = P
@@ -280,7 +284,7 @@ end
 """
   Return a fully balanced binary tree of given height.
 """
-function maximally_balanced(height; default_value=()->[0,0,0])
+function maximally_balanced(height; default_value=()->DefaultNodeValue([0,0,0]))
     P = [BinaryTree(default_value())]
     root = P[1]
     h = 1
@@ -296,8 +300,16 @@ function maximally_balanced(height; default_value=()->[0,0,0])
     return root
 end
 
+mutable struct PCNodeValue{T}
+    w::Float64
+    t::Int
+    observables::T
+end
+PCNodeValue(obs) = PCNodeValue(0.0, 0 , obs)
+PCNodeValue() = PCNodeValue(MVector(0,0,0))
+
 """
-    weighted_coalescent(n, w=randn(n).^2; default_value=[0, 0], fuse=max)
+    preferential_coalescent(n, w=randn(n).^2; default_value=[0, 0], fuse=max)
 
 Simulate a coalescent process for `n` genes, in which coalescing nodes are not selected uniformly, but according to the weight vector `w`.
 
@@ -306,16 +318,17 @@ Node values are set to `default_value`.
 
 Return a binary tree.
 """
-function weighted_coalescent(n, w=randn(n).^2; default_value::F = ()->Float64[0,0,0,0], fuse=max, stopat=1) where F
-    P = [BinaryTree(default_value()) for _ in 1:n]
+function preferential_coalescent(n, w=ones(n); fuse=max, stopat=1, nodevalue=()->PCNodeValue())
+    P = [BinaryTree(nodevalue()) for _ in 1:n]
     mask = trues(n)
     ws = WeightedSampler(w)
     d = ws.d
+    t = 1
     while n  > stopat
         i, j = ws_sample(ws, 2; ordered=true)
         l, r = P[i], P[j]
-        v = BinaryTree(default_value())
-        v.val[end-1] = max(l.val[end], r.val[end])
+        v = BinaryTree(nodevalue())
+        v.val.t = t
         v.left = l
         v.right = r
         l.parent = v
@@ -325,8 +338,9 @@ function weighted_coalescent(n, w=randn(n).^2; default_value::F = ()->Float64[0,
         new_weight = fuse(ws.heap[d+i], ws.heap[d+j])
         adjust_weight!(ws, i, new_weight)
         adjust_weight!(ws, j, 0.0)
-        v.val[end] = new_weight
+        v.val.w = new_weight
         n -= 1
+        t += 1
         mask[j] = false
     end
 
@@ -334,6 +348,14 @@ function weighted_coalescent(n, w=randn(n).^2; default_value::F = ()->Float64[0,
 end
 
 ## -- Niche model from Goldenfeld (2020) -- ##
+
+mutable struct NicheNodeValue{T}
+    n::Float64
+    t::Float64
+    observables::T
+end
+NicheNodeValue(obs) = NicheNodeValue(0.0, 0.0, obs)
+NicheNodeValue() = NicheNodeValue(MVector(0,0,0))
 
 e(r, R0) = r/(r+R0) # extinction probability
 r(n, ϵ=0.0) = ifelse(n>0, n, ϵ) # speciation rate
@@ -343,25 +365,27 @@ r(n, ϵ=0.0) = ifelse(n>0, n, ϵ) # speciation rate
 
 Minimal model of a speciation process coupled to fluctuating ecological niches.
 
-Return root node and total number of nodes in the final tree. The latter can be used
-to check if the tree generation process completed, or terminated prematurely.
+Return root node, number of generated nodes and number of nodes in the final tree after pruning.
+The middle value can be used to check if the tree generation process completed, or terminated prematurely.
 
 Reference: https://doi.org/10.1073/pnas.1915088117
 """
-function nichemodel(n, σ, R0=10.0; n0=1.0, ϵ=0.0, default_value=()->Float64[0.0, 0.0])
-    _default_value = () -> vcat(default_value(), [n0, 0.0])
+function nichemodel(n, σ, R0=10.0; n0=1.0, ϵ=0.0, nodevalue=()->NicheNodeValue())
     clock = 0.0
-    P = [BinaryTree(_default_value())] # root node
+    P = BinaryTree(nodevalue()) # root node
+    P.val.n = n0
     nnodes = 1
-    spec_queue = PriorityQueue(Base.Order.Forward, P[1] => 0.0)
+    npruned = 0
+    spec_queue = PriorityQueue(Base.Order.Forward, P => 0.0)
     while nnodes < n && !isempty(spec_queue)
         # Sample a node, speciate, and calculate niche sizes, spec. rates, ext. prob. 
         # of potential child nodes.
         # Advance clock to spec. event.
         p = dequeue!(spec_queue)
-        clock = p.val[end]
-        np = p.val[end-1]
-        nl, nr = np .+ np*σ*randn(2)
+        clock = p.val.t
+        np = p.val.n
+        nl = np + np*σ*randn()
+        nr = np + np*σ*randn()
         rl, rr = r(nl, ϵ), r(nr, ϵ)
         tl = clock - log(rand())/rl
         tr = clock - log(rand())/rr
@@ -373,18 +397,18 @@ function nichemodel(n, σ, R0=10.0; n0=1.0, ϵ=0.0, default_value=()->Float64[0.
         if rand() > el
             n_children += 1
             nnodes += 1
-            left = child!(p, _default_value(), :left)
-            left.val[end-1] = nl
-            left.val[end] = tl
+            left = child!(p, nodevalue(), :left)
+            left.val.n = nl
+            left.val.t = tl
             !isinf(tl) && enqueue!(spec_queue, left => tl)
         end
         if nnodes<n && rand() > er
             n_children += 1
             location = n_children == 2 ? :right : :left
             nnodes += 1
-            right = child!(p, _default_value(), location)
-            right.val[end-1] = nr
-            right.val[end] = tr
+            right = child!(p, nodevalue(), location)
+            right.val.n = nr
+            right.val.t = tr
             !isinf(tr) && enqueue!(spec_queue, right => tr)
         end
 
@@ -401,7 +425,7 @@ function nichemodel(n, σ, R0=10.0; n0=1.0, ϵ=0.0, default_value=()->Float64[0.
                 pa.left = nothing
                 pa.right = nothing
                 sib.parent = nothing
-                P[1] = sib
+                P = sib
                 continue
             end
             if isleftchild(pa)
@@ -413,13 +437,13 @@ function nichemodel(n, σ, R0=10.0; n0=1.0, ϵ=0.0, default_value=()->Float64[0.
             pa.left = nothing
             pa.right = nothing
             pa.parent = nothing
-            # nnodes -= 2
+            npruned += 2
         end
 
         if n_children == 1
             chld = p.left
             if isnothing(pa)
-                P[1] = chld
+                P = chld
                 chld.parent = nothing
             end
             chld.parent = pa
@@ -430,12 +454,12 @@ function nichemodel(n, σ, R0=10.0; n0=1.0, ϵ=0.0, default_value=()->Float64[0.
                 chld.parent = pa
             end
             p.left = p.right = p.parent = nothing
-            # nnodes -= 1
+            npruned += 1
         end
     end
     # returning the number of nodes serves as a check whether the tree was constructed fully,
     # or the process halted prematurely.
-    return P[1], nnodes
+    return P, nnodes, nnodes-npruned
 end
 
 end # MODULE
